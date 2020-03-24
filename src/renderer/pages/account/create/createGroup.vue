aed3b6ccd1b35856877933b6243e9b7c56788da5
winter520 <WDwq681227>
2020-03-24 19:13:59 +0800

    repair create valid

--- src/renderer/pages/account/create/createGroup.vue
+++ src/renderer/pages/account/create/createGroup.vue
@@ -3,32 +3,30 @@
 
     <div :class="formBoxClass ? 'c-form-box' : 'c-form-box-sm'">
       <div class="WW100">
-        <el-form ref="groupForm" :rules="rules" label-width="100px" label-position="top" @submit.native.prevent>
-          <el-form-item :label="$t('label').mode"
-            :rules="{
-              required: true, message: $t('warn').w_5, trigger: 'blur'
-            }">
+        <el-form ref="node" :rules="rules" label-width="100px" label-position="top" @submit.native.prevent>
+          <el-form-item :label="$t('label').mode" >
             <el-select v-model="mode.select" :placeholder="$t('warn').w_4" class="WW100">
               <el-option v-for="(item, index) in mode.init" :key="index" :label="item.name + ' ' + $t('label').mode" :value="item.val"></el-option>
             </el-select>
           </el-form-item>
           <el-form-item
-            v-for="(item, index) in node.select" :label="$t('label').admins + (index + 1)" :key="index" :prop="'eNode.' + index + '.value'"
-            :rules="{
-              required: true, message: $t('warn').w_7, trigger: 'blur'
-            }"
+            v-for="(item, index) in node.select" :key="index" :prop="'eNode.' + index + '.value'"
           >
+            <div slot="label" class="flex-sc">
+              <span class="color_red">*</span>
+              {{$t('label').admins + (index + 1)}}
+              <div class="ml-20" v-if="node.refresh">
+                <span class="color_green" v-if="item.state === 'OnLine'"><i class="el-icon-circle-check mr-5"></i>{{$t('state').on}}</span>
+                <span class="color_red" v-if="item.state === 'OffLine'"><i class="el-icon-circle-close mr-5"></i>{{$t('state').off}}</span>
+              </div>
+            </div>
             <div class="flex-bc">
               <el-input v-model="item.value" @blur="changeState(item, index)" :disabled="item.value === eNode || item.isSelf ? true : false" :title="item.value"></el-input>
             </div>
-            <div class="flex-sc" v-if="node.refresh">
-              <span class="color_green" v-if="item.state === 'OnLine'"><i class="el-icon-circle-check mr-5"></i>{{$t('state').on}}</span>
-              <span class="color_red" v-if="item.state === 'OffLine'"><i class="el-icon-circle-close mr-5"></i>{{$t('state').off}}</span>
-            </div>
           </el-form-item>
           <el-form-item class="mt-30">
-            <el-button type="primary" native-type="submit" @click="submitForm('groupForm')">{{$t('btn').createAccount}}</el-button>
-            <el-button @click="resetForm('groupForm')">{{$t('btn').restart}}</el-button>
+            <el-button type="primary" native-type="submit" @click="submitForm('node')">{{$t('btn').createAccount}}</el-button>
+            <el-button @click="resetForm('node')">{{$t('btn').restart}}</el-button>
             <!-- <el-button @click="toUrl('/fsgsd')">返回</el-button> -->
           </el-form-item>
         </el-form>
@@ -104,14 +102,27 @@ export default {
   methods: {
     ...methods,
     submitForm(formName) {
-      this.$refs[formName].validate((valid) => {
-        if (valid) {
-          this.eDialog.confirm = true
-        } else {
-          console.log('error submit!!');
-          return false;
+      console.log(this.node.select)
+      let _flag = false
+      for (let obj of this.node.select) {
+        if (!obj.value) {
+          _flag = true
+          break
         }
-      });
+      }
+      if (_flag) {
+        this.msgError(this.$t('warn').w_7)
+        return
+      }
+      this.eDialog.confirm = true
+      // this.$refs[formName].validate((valid) => {
+      //   if (valid) {
+      //     this.eDialog.confirm = true
+      //   } else {
+      //     console.log('error submit!!');
+      //     return false;
+      //   }
+      // })
     },
     createGroup () {
       let arr = [], signStr = ''
@@ -161,7 +172,10 @@ export default {
       AddGroupAccountsFn(this, 'GroupAccountsAdd', data)
     },
     changeState (item, index) {
-      if (!item.value) return
+      if (!item.value) {
+        this.msgError(this.$t('warn').w_7)
+        return
+      }
       let enode = item.value
       let enodeArr = this.node.select
       if (enode.indexOf('enode:') < 0 || enode.indexOf('@') < 0 ) {
