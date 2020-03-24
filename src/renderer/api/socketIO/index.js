bf6f1647a066fb7f37afd9743cd1326464eb2ec6
winter520 <WDwq681227>
2020-03-24 19:22:01 +0800

    change connected

--- src/renderer/api/socketIO/index.js
+++ src/renderer/api/socketIO/index.js
@@ -2,11 +2,8 @@
 let count = 0
 const SocketIO = (that, url, params) => {
   return new Promise((resolve, reject) => {
-    // console.log(that.$socket.connected)
-    // console.log(url)
-    // console.log(params)
     if (that.$socket.connected) {
-      that.sockets.unsubscribe(url)
+      // that.sockets.unsubscribe(url)
       that.$socket.emit(url, params)
       that.sockets.subscribe(url, res => {
         // console.log(res)
